781b412605164abb03a8b591775e84f25b28c137
csxtan <csxtan@gmail.com>
2017-06-15 15:36:25 -0700

    Added overlay option and other small changes
    Graph overlay option which takes a microscopy image
    Error catchment and improved usage help
    Support for only analyzing one channel
    Fixed y-axis indexing
    Directory to contain outputs

--- microscopy_neighbours.py
+++ microscopy_neighbours.py
@@ -2,49 +2,86 @@
 
 """
 Author: Cindy Tan
-Last Modified Date: 9 June 2017
+Last Modified Date: 15 June 2017
 
 Processes a MATLAB simulation csv file and determines cell neighbours at a specific timeframe
 Arguments:
-    -c CellProfiler - add flag if data is from CellProfiler (as opposed to MATLAB, default)
-    -g, -r - path to green and red CSV files
-    -t timeframe - time at which cells are analyzed, specified as frame number
-    -m or -p - radius in microns or pixels
-    -d print distance - add flag if graph should be annotated with distances
+    -c CellProfiler: add flag if data is from CellProfiler (as opposed to MATLAB, default)
+    -g, -r: path to green and red CSV files (at least one file must be provided)
+    -t timeframe: time at which cells are analyzed, specified as frame number
+    -m or -p: radius in microns or pixels
+    -d print distance: add flag if graph should be annotated with distances
+    -o overlay: add flag if graph should be overlaid on a background image (provide image path as argument)
 Output: determines cell neighbours from radius, produces a graph of cells and neighbours and histogram of # of cells vs.
 # of neighbours, saves both images
 """
 
+import os
+import sys
 import math
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from PIL import Image
 from optparse import OptionParser
 from graphviz import Graph
 
 # Options parsing
-parser = OptionParser()
-parser.add_option('-c', '--cellprofiler', action='store_true', dest='cp', help='use CellProfiler file instead of MATLAB')
-parser.add_option('-g', '--greenfile', type='string', dest='green_file', help='path to green CSV file')
-parser.add_option('-r', '--redfile', type='string', dest='red_file', help='path to red CSV file')
-parser.add_option('-t', '--time', type='int', dest='time', help='timeframe at which cells are analyzed')
-parser.add_option('-m', '--radius_m', type='string', dest='radius_m', help='radius in microns')
-parser.add_option('-p', '--radius_p', type='string', dest='radius_p', help='radius in pixels')
-parser.add_option('-d', '--printdist', action='store_true', dest='printdist', help='print distance on edges')
+usage = '%prog options\n  > at least one file (-r/-g) must be specified\n  > exactly one radius parameter (-m/-p)' \
+        ' must be specified\n  > time (-t) must be specified\n  > all other options are not required'
+parser = OptionParser(usage=usage)
+parser.add_option('-g', type='string', dest='green_file', help='path to green CSV file')
+parser.add_option('-r', type='string', dest='red_file', help='path to red CSV file')
+parser.add_option('-t', type='int', dest='time', help='timeframe at which cells are analyzed')
+parser.add_option('-m', type='string', dest='radius_m', help='radius in microns')
+parser.add_option('-p', type='string', dest='radius_p', help='radius in pixels')
+parser.add_option('-c', action='store_true', dest='cp', help='use CellProfiler file instead of MATLAB')
+parser.add_option('-d', action='store_true', dest='printdist', help='print distance on edges')
+parser.add_option('-o', type='string', dest='overlay', help='use graph as overlay, argument is path to background image file')
 
 (options, args) = parser.parse_args()
+error = 0
+if options.printdist:
+    printdist = options.printdist
+else:
+    printdist = 0
 if options.cp:
     microns_per_pixel = 0.8
 else:
-    microns_per_pixel = 0.4
+    microns_per_pixel = 0.8
 if options.radius_p:
     radius = float(options.radius_p) * microns_per_pixel
 elif options.radius_m:
     radius = float(options.radius_m)
-printdist = 0
-if options.printdist:
-    printdist = options.printdist
+else:
+    error = 1
+    print 'Error: no radius specified, use -m or -p'
+if not(options.green_file or options.red_file):
+    error = 1
+    print 'Error: no file input, use -g and/or -r'
+if not(options.time):
+    error = 1
+    print 'Error: no timeframe specified, use -t'
+if error:
+    sys.exit('Use -h for options usage help')
+
+# Store output information in a string
+if options.green_file and options.red_file:
+    info = '_r' + str(int(radius)) + '_t' + str(options.time)
+else:
+    if options.green_file:
+        info = '_green_r' + str(int(radius)) + '_t' + str(options.time)
+    else:
+        info = '_red_r' + str(int(radius)) + '_t' + str(options.time)
+
+# Build directory to contain outputs
+directory = 'microscopy_neighbours' + info
+directory_i = 0
+while os.path.exists(directory):
+    directory_i += 1
+    directory = 'microscopy_neighbours' + info + '_' + str(directory_i)
+os.makedirs(directory)
 
 # Data extraction
 def parse_csv(file):
@@ -55,33 +92,43 @@ def parse_csv(file):
     else:
         frame_col = 'Frame'
         data_cols = ['CellID', 'CentroidX', 'CentroidY']
-    indices = np.where(df[frame_col] == options.time)[0]
+    try:
+        indices = np.where(df[frame_col] == options.time)[0]
+    except KeyError:
+        sys.exit('Error: could not parse CSV, use -c if file is for CellProfiler (MATLAB files are default)\n'
+                 'Use -h for options usage help')
     df = df[indices[0]:indices[-1] + 1][data_cols]
     df.columns = ['Cell', 'X', 'Y']
     return df
 
-green_data, red_data = parse_csv(options.green_file), parse_csv(options.red_file)
-split = green_data.shape[0]
-data = pd.concat([green_data, red_data])
+if options.green_file and options.red_file:
+    green_data, red_data = parse_csv(options.green_file), parse_csv(options.red_file)
+    split = green_data.shape[0]
+    data = pd.concat([green_data, red_data])
+else:
+    if options.green_file:
+        data = parse_csv(options.green_file)
+        split = data.shape[0]
+    else:
+        data = parse_csv(options.red_file)
+        split = 0
 
 count = np.zeros(data.shape[0])
 
 # Graphviz setup
 scale = 0.025
-def name():
-    return 'neighbours_r' + str(int(radius)) + '_t' + str(options.time)
 
 redf = '#FF9090'
 greenf = '#75CF90'
 redl = '#FF3535'
 greenl = '#019A2F'
 
-g = Graph(name=name(), format='png', engine='neato')
+g = Graph(name='cellgraph' + info, format='png', engine='neato')
 
 g.attr('node', pin='true', shape='circle', style='filled', width='0.5', fixedsize='true', fontname='courier')
 g.attr('edge', fontsize='10.0', penwidth='2', fontname='courier')
 
-# Plot graph and collect histogram data
+# Create graph and collect histogram data
 i = 0
 while i < data.shape[0]:
     def cell_type(index, out='int'):
@@ -104,7 +151,7 @@ while i < data.shape[0]:
         return cell_type(index, 'str') + str(int(data.iloc[index]['Cell']))
 
     x, y = data.iloc[i]['X'] * microns_per_pixel, data.iloc[i]['Y'] * microns_per_pixel
-    position = str(x * scale) + ',' + str(y * scale)
+    position = str(x * scale) + ',' + str(y * scale * -1)
 
     g.node(cell_name(i), pos=position, fillcolor=cell_type(i, 'hex'))
 
@@ -132,9 +179,12 @@ while i < data.shape[0]:
 
     i += 1
 
+# Generate graph image
+g.render(directory + '/cellgraph' + info + '.gv')
+
+# Plot and generate histogram image
 count = np.split(count, [split])
 
-# Plot histogram
 plt.hist([count[0], count[1]], np.arange(min(np.concatenate((count[0], count[1]))) - 0.5,
                                          max(np.concatenate((count[0], count[1]))) + 1.5, 1),
          lw=0, color=[greenf, redf], label=['Green', 'Red'])
@@ -144,6 +194,26 @@ plt.xlim([min(np.concatenate((count[0], count[1]))) - 0.5, max(np.concatenate((c
 plt.xlabel('# of neighbours')
 plt.ylabel('# of cells')
 plt.axes().yaxis.grid()
-plt.savefig(name() + '.png')
+plt.savefig(directory + '/neighbourhist' + info + '.png')
 
-g.render()
+# Overlay image
+if options.overlay:
+    image = Image.open(options.overlay)
+    graph = Image.open(directory + '/cellgraph' + info + '.gv.png')
+    ratio = float(image.size[0]) / image.size[1]
+
+    image = image.resize((int(round(graph.size[1] * ratio)), graph.size[1]))
+
+    mask = Image.new('L', graph.size, color=0)
+
+    bands = list(graph.split())
+    if len(bands) == 3:
+        mask = Image.new('L', graph.size, color=128)
+        graph.putalpha(mask)
+        bands = list(graph.split())
+    else:
+        bands[3] = bands[3].point(lambda x: x * 0.5)
+    graph = Image.merge(graph.mode, bands)
+    image.paste(graph, (0, 0), graph)
+
+    image.save(directory + '/cellgraph_overlay' + info + '.png')
