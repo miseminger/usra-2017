5b3644b05f866c4586414f537218fffaca347c9b
miseminger <32916719+miseminger@users.noreply.github.com>
2018-02-22 21:24:40 -0800

    Create leadingedge.py

---
+++ leadingedge.py
@@ -0,0 +1,7 @@
+adapt the csv for neighbours to include cell positions and velocities
+
+#for now, assume the cells are all moving in the x direction
+#find the cell furthest to the right
+#take all cells with x values from the furthest right to distance d to the left of it
+#have the option to take several (n) strips of cells: total cell group width/n
+#for each strip, labeled 1,2,3,4,...,n, find the neighbours 
